669b2c9d4ebec837b1f630102a519ed683918682
Ameya D <ameyad@Ameyas-MacBook-Air.local>
2021-10-29 04:22:03 -0700

    changed-model

--- models/basic_cnn.py
+++ models/basic_cnn.py
@@ -8,12 +8,12 @@ class ConvNet(nn.Module):
     def __init__(self, num_classes=120):
         super(ConvNet, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(3, 6, kernel_size=5, stride=1, padding=0),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(6, 32, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
