00c3548eb97899a4f1673c9e522d08765fc9ac5a
Ameya D <ameyad@Ameyas-MacBook-Air.local>
2021-10-29 06:01:37 -0700

    added-plot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools
 import os
 import random
 
@@ -13,9 +14,10 @@ from torchvision import transforms
 from torch.autograd import Variable
 import time
 import dataloader
-#import models.basic_cnn
+import models.basic_cnn
 import matplotlib.pyplot as plt
-from torchvision import models
+#from torchvision import models
+from sklearn.metrics import confusion_matrix, classification_report
 
 SEED = 1234
 
@@ -26,6 +28,31 @@ torch.cuda.manual_seed(SEED)
 torch.manual_seed(7)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('confusionmatrix.png')
 
 def plot_training_statistics(train_stats, model_name):
     fig, axes = plt.subplots(2, figsize=(15, 15))
@@ -156,12 +183,12 @@ def main(args):
     print(f'Number of validation examples: {len(valid_data)}')
     print(f'Number of testing examples: {len(test_data)}')
 
-    #model = models.basic_cnn.Net().to(device)
-    model = models.resnet18(pretrained=True).to(device)
-    for name, param in model.named_parameters():
-        if ("bn" not in name):
-            param.requires_grad = False
-    model.fc = nn.Linear(model.fc.in_features, 120).to(device)
+    model = models.basic_cnn.Net().to(device)
+    # model = models.resnet18(pretrained=True).to(device)
+    # for name, param in model.named_parameters():
+    #     if ("bn" not in name):
+    #         param.requires_grad = False
+    # model.fc = nn.Linear(model.fc.in_features, 120).to(device)
 
 
     # Loss and optimizer
@@ -203,6 +230,8 @@ def main(args):
     with torch.no_grad():
         correct = 0
         total = 0
+        all_labels = []
+        all_preds = []
         for images, labels in test_loader:
             images = images.to(device)
             labels = labels.to(device)
@@ -210,10 +239,19 @@ def main(args):
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
+            all_labels.extend(labels)
+            all_preds.extend(predicted)
+
 
         print('Test Accuracy of the model on the 2000 test images: {} %'.format(100 * correct / total))
-
-
+        classes = ['affenpinscher','afghan_hound','african_hunting_dog','airedale','american_staffordshire_terrier','appenzeller','australian_terrier','basenji','basset','beagle','bedlington_terrier','bernese_mountain_dog','black-and-tan_coonhound','blenheim_spaniel','bloodhound','bluetick','border_collie','border_terrier','borzoi','boston_bull','bouvier_des_flandres','boxer','brabancon_griffon','briard','brittany_spaniel','bull_mastiff','cairn','cardigan','chesapeake_bay_retriever','chihuahua','chow','clumber','cocker_spaniel','collie','curly-coated_retriever','dandie_dinmont','dhole','dingo','doberman','english_foxhound','english_setter','english_springer','entlebucher','eskimo_dog','flat-coated_retriever','french_bulldog','german_shepherd','german_short-haired_pointer','giant_schnauzer','golden_retriever','gordon_setter','great_dane','great_pyrenees','greater_swiss_mountain_dog','groenendael','ibizan_hound','irish_setter','irish_terrier','irish_water_spaniel','irish_wolfhound','italian_greyhound','japanese_spaniel','keeshond','kelpie','kerry_blue_terrier','komondor','kuvasz','labrador_retriever','lakeland_terrier','leonberg','lhasa','malamute','malinois','maltese_dog','mexican_hairless','miniature_pinscher','miniature_poodle','miniature_schnauzer','newfoundland','norfolk_terrier','norwegian_elkhound','norwich_terrier','old_english_sheepdog','otterhound','papillon','pekinese','pembroke','pomeranian','pug','redbone','rhodesian_ridgeback','rottweiler','saint_bernard','saluki','samoyed','schipperke','scotch_terrier','scottish_deerhound','sealyham_terrier','shetland_sheepdog','shih-tzu','siberian_husky','silky_terrier','soft-coated_wheaten_terrier','staffordshire_bullterrier','standard_poodle','standard_schnauzer','sussex_spaniel','tibetan_mastiff','tibetan_terrier','toy_poodle','toy_terrier','vizsla','walker_hound','weimaraner','welsh_springer_spaniel','west_highland_white_terrier','whippet','wire-haired_fox_terrier','yorkshire_terrier']
+        print(classification_report(all_labels, all_preds, target_names=classes))
+        all_pred = torch.tensor(all_preds)
+        all_label = torch.tensor(all_labels)
+        confusion_mat = confusion_matrix(y_true=all_label, y_pred=all_pred)
+        print(confusion_mat)
+        plot_confusion_matrix(cm=confusion_matrix(y_true=all_label, y_pred=all_pred),
+                              classes=classes)
 
 
 if __name__ == '__main__':
